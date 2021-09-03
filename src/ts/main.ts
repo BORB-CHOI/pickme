cea4e5e23705571d0d59657b9034f7ae3f10f3bd
BORB <qzsec299@naver.com>
2021-09-03 17:18:41 +0900

    feat : text edit and size edit

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,23 +1,53 @@
-import { isInterfaceDeclaration } from "typescript";
 // import { random } from "./util";
+
+import { Word } from "./util";
 
 const P = document.querySelector(".p");
 
-const handleClickP = (event:any):any =>{
-  const target =  event.currentTarget;
+const handleClickP = (event: any): any => {
+  const target = event.currentTarget;
   console.dir(target);
-  target.textContent = 'Pick me';
-  target.style.fontSize = '30vh';
-  add
 
+  setTimeout(() => {
+    target.textContent = "Pi";
+    target.style.fontSize = "90vh";
+  }, 100);
+  setTimeout(() => {
+    target.textContent = "Pic";
+    target.style.fontSize = "80vh";
+  }, 150);
+  setTimeout(() => {
+    target.textContent = "Pick";
+    target.style.fontSize = "60vh";
+  }, 200);
+  setTimeout(() => {
+    target.textContent = "Pick m";
+    target.style.fontSize = "40vh";
+  }, 250);
+  setTimeout(() => {
+    target.textContent = "Pick me";
+    target.style.fontSize = "30vh";
+  }, 300);
+  setTimeout(() => {
+    target.textContent = "Pick_me";
+    target.style.fontSize = "20vh";
+    target.style.transform = "rotate(45deg)";
+  }, 350);
+  // const word_i = new Word();
+  // const word_c = new Word();
+  // const word_k = new Word();
+  // const word_m = new Word();
+  // const word_e = new Word();
+
+  P.removeEventListener("click", handleClickP);
 };
 
-const handleEvent = ():any => {
+const handleEvent = (): any => {
   console.log(P);
-  P.addEventListener('click', handleClickP);
+  P.addEventListener("click", handleClickP);
 };
 
-const init = ():any => {
+const init = (): any => {
   handleEvent();
 };
 
--- src/ts/util.ts
+++ src/ts/util.ts
@@ -1,1 +1,15 @@
 export const random = (max: number): number => Math.floor(Math.random() * max);
+
+// Will create interface.
+interface WordValue {
+  word: string;
+  fontSize: number;
+}
+
+export class Word {
+  constructor() {}
+
+  addWord = () => {};
+}
+
+// Will create Add Function.
